770b79296182546de402b8235222d5cb8f88ff7e
Animash Roy <47457885+animash-cse@users.noreply.github.com>
2019-07-21 19:15:08 +0600

    Upload all chat app files

---
+++ app/src/main/java/com/animash/chat/app/bd/WelcomeSlide/WelcomeActivity.java
@@ -0,0 +1,18 @@
+package com.animash.chat.app.bd.WelcomeSlide;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(WelcomeActivity.this, IntroActivity.class));
+        finish();
+
+    }
+
+}
